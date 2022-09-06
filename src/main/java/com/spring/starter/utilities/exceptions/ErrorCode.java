df065614f400f4cc257e32907d6f325a40bbf22e
Pedro Dias <pedro158a@gmail.com>
2022-09-06 19:46:13 +0100

    Remove javax dependency

--- src/main/java/com/spring/starter/utilities/exceptions/ErrorCode.java
+++ src/main/java/com/spring/starter/utilities/exceptions/ErrorCode.java
@@ -1,11 +1,11 @@
 package com.spring.starter.utilities.exceptions;
 
-import javax.ws.rs.core.Response;
+import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-    FILE_TYPE_NOT_SUPPORTED("File type not supported", Response.Status.BAD_REQUEST.getStatusCode()),
-    FILE_ZONES_FRIST_IMPORT("Zones file must be the first import", Response.Status.BAD_REQUEST.getStatusCode()),
-    ZONE_NOT_FOUND("Zone not found", Response.Status.BAD_REQUEST.getStatusCode());
+    FILE_TYPE_NOT_SUPPORTED("File type not supported", HttpStatus.BAD_REQUEST.value()),
+    FILE_ZONES_FRIST_IMPORT("Zones file must be the first import", HttpStatus.BAD_REQUEST.value()),
+    ZONE_NOT_FOUND("Zone not found", HttpStatus.BAD_REQUEST.value());
 
     private final String message;
     private final int httpStatusCode;
